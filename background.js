1f4dd82b7791405cf709e2794b72ac2da239090e
Svante Richter <svanter@axis.com>
2017-07-25 18:10:29 +0200

    Updating to es6 stuff
    Change-Id: Iaf99e7a397de76031bde7bd660fbaf860db3162c

--- background.js
+++ background.js
@@ -1,97 +1,128 @@
-var events = [];
-var active = false;
-var contextItems;
+let events = [];
+let active = false;
+let contextItems;
 chrome.runtime.onMessage.addListener(function (request, sender, response) {
-  if (request.action == "get_events") {
-    response({ 'events': events });
-  }
-  if (request.events) {
-    events = request.events;
-  }
+    if (request.action == "get_events") {
+        response({
+            'events': events
+        });
+    }
+    if (request.events) {
+        events = request.events;
+    }
 });
 
 function createContextItems() {
-  return {
-    comment: chrome.contextMenus.create({
-      "title": "Add comment",
-      "onclick": function () {
-        chrome.tabs.getSelected(null, function (tab) {
-          var text = window.prompt("Type your comment:");
-          chrome.tabs.sendMessage(tab.id, { action: "addComment", 'text': text });
-        });
-      }
-    }),
+    return {
+        comment: chrome.contextMenus.create({
+            "title": "Add comment",
+            "onclick": function () {
+                chrome.tabs.getSelected(null, function (tab) {
+                    const text = window.prompt("Type your comment:");
+                    chrome.tabs.sendMessage(tab.id, {
+                        action: "addComment",
+                        'text': text
+                    });
+                });
+            }
+        }),
 
-    screenshot: chrome.contextMenus.create({
-      "title": "Take screenshot",
-      "onclick": function () {
-        chrome.tabs.getSelected(null, function (tab) {
-          chrome.tabs.sendMessage(tab.id, { action: "takeScreenshot" });
-        });
-      }
-    }),
+        screenshot: chrome.contextMenus.create({
+            "title": "Take screenshot",
+            "onclick": function () {
+                chrome.tabs.getSelected(null, function (tab) {
+                    chrome.tabs.sendMessage(tab.id, {
+                        action: "takeScreenshot"
+                    });
+                });
+            }
+        }),
 
-    checkValue: chrome.contextMenus.create({
-      "title": "Check value",
-      "onclick": function (e) {
-        chrome.tabs.getSelected(null, function (tab) {
-          chrome.tabs.sendMessage(tab.id, { action: "checkValue" });
-        });
-      }
-    }),
+        checkValue: chrome.contextMenus.create({
+            "title": "Check value",
+            "onclick": function (e) {
+                chrome.tabs.getSelected(null, function (tab) {
+                    chrome.tabs.sendMessage(tab.id, {
+                        action: "checkValue"
+                    });
+                });
+            }
+        }),
 
-    checkText: chrome.contextMenus.create({
-      "title": "Check text",
-      "contexts": ["selection"],
-      "onclick": function (e) {
-        chrome.tabs.getSelected(null, function (tab) {
-          chrome.tabs.sendMessage(tab.id, { action: "checkText", text: e.selectionText });
-        });
-      }
-    }),
+        checkText: chrome.contextMenus.create({
+            "title": "Check text",
+            "contexts": ["selection"],
+            "onclick": function (e) {
+                chrome.tabs.getSelected(null, function (tab) {
+                    chrome.tabs.sendMessage(tab.id, {
+                        action: "checkText",
+                        text: e.selectionText
+                    });
+                });
+            }
+        }),
 
-    checkImage: chrome.contextMenus.create({
-      "title": "Check image",
-      "contexts": ["image"],
-      "onclick": function (e) {
-        chrome.tabs.getSelected(null, function (tab) {
-          chrome.tabs.sendMessage(tab.id, { action: "checkImage" });
-        });
-      }
-    }),
+        checkImage: chrome.contextMenus.create({
+            "title": "Check image",
+            "contexts": ["image"],
+            "onclick": function (e) {
+                chrome.tabs.getSelected(null, function (tab) {
+                    chrome.tabs.sendMessage(tab.id, {
+                        action: "checkImage"
+                    });
+                });
+            }
+        }),
 
-    checkLink: chrome.contextMenus.create({
-      "title": "Check link",
-      "contexts": ["link"],
-      "onclick": function (e) {
-        chrome.tabs.getSelected(null, function (tab) {
-          chrome.tabs.sendMessage(tab.id, { action: "checkLink" });
-        });
-      }
-    })
-  }
+        checkLink: chrome.contextMenus.create({
+            "title": "Check link",
+            "contexts": ["link"],
+            "onclick": function (e) {
+                chrome.tabs.getSelected(null, function (tab) {
+                    chrome.tabs.sendMessage(tab.id, {
+                        action: "checkLink"
+                    });
+                });
+            }
+        })
+    }
 }
 
-var record = chrome.contextMenus.create({
-  title: "Start recording",
-  onclick: function (info, tab) {
-    if (!active) {
-      chrome.contextMenus.update(record, { title: "Stop recording" });
-      active = true;
-      events = [];
-      contextItems = createContextItems();
-      chrome.tabs.update(tab.id, { url: tab.url }, function (tab) {
-        chrome.tabs.sendMessage(tab.id, { action: "start", 'url': tab.url });
-      });
-    } else {
-      chrome.contextMenus.update(record, { title: "Start recording" });
-      Object.values(contextItems).forEach(function (name) {
-        chrome.contextMenus.remove(name);
-      });
-      chrome.tabs.sendMessage(tab.id, { action: "stop" });
-      active = false;
+const record = chrome.contextMenus.create({
+    title: "Start recording",
+    onclick: function (info, tab) {
+        if (!active) {
+            chrome.contextMenus.update(record, {
+                title: "Stop recording"
+            });
+            active = true;
+            events = [];
+            contextItems = createContextItems();
+            chrome.tabs.update(tab.id, {
+                url: tab.url
+            }, function (tab) {
+                chrome.tabs.sendMessage(tab.id, {
+                    action: "start",
+                    'url': tab.url
+                });
+            });
+        } else {
+            chrome.contextMenus.update(record, {
+                title: "Start recording"
+            });
+            Object.values(contextItems).forEach(function (name) {
+                chrome.contextMenus.remove(name);
+            });
+            chrome.tabs.sendMessage(tab.id, {
+                action: "stop"
+            });
+            active = false;
+            chrome.notifications.create({
+                type: "basic",
+                iconUrl: "./recorder.png",
+                title: "Casperjs recorder",
+                message: "Casperjs test is now in your clipboard."
+            });
+        }
     }
-  }
 });
-
-
--- recorder.js
+++ recorder.js
@@ -1,222 +1,3 @@
-
-
-
-
-/*
- * Copyright (C) 2015 Pavel Savshenko
- * Copyright (C) 2011 Google Inc.  All rights reserved.
- * Copyright (C) 2007, 2008 Apple Inc.  All rights reserved.
- * Copyright (C) 2008 Matt Lilek <webkit@mattlilek.com>
- * Copyright (C) 2009 Joseph Pecoraro
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- * 2.  Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
- *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-var UTILS = {};
-UTILS.cssPath = function (node, optimized) {
-    if (node.nodeType !== Node.ELEMENT_NODE)
-        return "";
-    var steps = [];
-    var contextNode = node;
-    while (contextNode) {
-        var step = UTILS._cssPathStep(contextNode, !!optimized, contextNode === node);
-        if (!step)
-            break; // Error - bail out early.
-        steps.push(step);
-        if (step.optimized)
-            break;
-        contextNode = contextNode.parentNode;
-    }
-    steps.reverse();
-    return steps.join(" > ");
-}
-UTILS._cssPathStep = function (node, optimized, isTargetNode) {
-    if (node.nodeType !== Node.ELEMENT_NODE)
-        return null;
-
-    var id = node.getAttribute("id");
-    if (optimized) {
-        if (id)
-            return new UTILS.DOMNodePathStep(idSelector(id), true);
-        var nodeNameLower = node.nodeName.toLowerCase();
-        if (nodeNameLower === "body" || nodeNameLower === "head" || nodeNameLower === "html")
-            return new UTILS.DOMNodePathStep(node.nodeName.toLowerCase(), true);
-    }
-    var nodeName = node.nodeName.toLowerCase();
-
-    if (id)
-        return new UTILS.DOMNodePathStep(nodeName.toLowerCase() + idSelector(id), true);
-    var parent = node.parentNode;
-    if (!parent || parent.nodeType === Node.DOCUMENT_NODE)
-        return new UTILS.DOMNodePathStep(nodeName.toLowerCase(), true);
-
-    /**
-     * @param {UTILS.DOMNode} node
-     * @return {Array.<string>}
-     */
-    function prefixedElementClassNames(node) {
-        var classAttribute = node.getAttribute("class");
-        if (!classAttribute)
-            return [];
-
-        return classAttribute.split(/\s+/g).filter(Boolean).map(function (name) {
-            // The prefix is required to store "__proto__" in a object-based map.
-            return "$" + name;
-        });
-    }
-
-    /**
-     * @param {string} id
-     * @return {string}
-     */
-    function idSelector(id) {
-        return "#" + escapeIdentifierIfNeeded(id);
-    }
-
-    /**
-     * @param {string} ident
-     * @return {string}
-     */
-    function escapeIdentifierIfNeeded(ident) {
-        if (isCSSIdentifier(ident))
-            return ident;
-        var shouldEscapeFirst = /^(?:[0-9]|-[0-9-]?)/.test(ident);
-        var lastIndex = ident.length - 1;
-        return ident.replace(/./g, function (c, i) {
-            return ((shouldEscapeFirst && i === 0) || !isCSSIdentChar(c)) ? escapeAsciiChar(c, i === lastIndex) : c;
-        });
-    }
-
-    /**
-     * @param {string} c
-     * @param {boolean} isLast
-     * @return {string}
-     */
-    function escapeAsciiChar(c, isLast) {
-        return "\\" + toHexByte(c) + (isLast ? "" : " ");
-    }
-
-    /**
-     * @param {string} c
-     */
-    function toHexByte(c) {
-        var hexByte = c.charCodeAt(0).toString(16);
-        if (hexByte.length === 1)
-            hexByte = "0" + hexByte;
-        return hexByte;
-    }
-
-    /**
-     * @param {string} c
-     * @return {boolean}
-     */
-    function isCSSIdentChar(c) {
-        if (/[a-zA-Z0-9_-]/.test(c))
-            return true;
-        return c.charCodeAt(0) >= 0xA0;
-    }
-
-    /**
-     * @param {string} value
-     * @return {boolean}
-     */
-    function isCSSIdentifier(value) {
-        return /^-?[a-zA-Z_][a-zA-Z0-9_-]*$/.test(value);
-    }
-
-    var prefixedOwnClassNamesArray = prefixedElementClassNames(node);
-    var needsClassNames = false;
-    var needsNthChild = false;
-    var ownIndex = -1;
-    var siblings = parent.children;
-    for (var i = 0; (ownIndex === -1 || !needsNthChild) && i < siblings.length; ++i) {
-        var sibling = siblings[i];
-        if (sibling === node) {
-            ownIndex = i;
-            continue;
-        }
-        if (needsNthChild)
-            continue;
-        if (sibling.nodeName.toLowerCase() !== nodeName.toLowerCase())
-            continue;
-
-        needsClassNames = true;
-        var ownClassNames = prefixedOwnClassNamesArray;
-        var ownClassNameCount = 0;
-        for (var name in ownClassNames)
-            ++ownClassNameCount;
-        if (ownClassNameCount === 0) {
-            needsNthChild = true;
-            continue;
-        }
-        var siblingClassNamesArray = prefixedElementClassNames(sibling);
-        for (var j = 0; j < siblingClassNamesArray.length; ++j) {
-            var siblingClass = siblingClassNamesArray[j];
-            if (ownClassNames.indexOf(siblingClass))
-                continue;
-            delete ownClassNames[siblingClass];
-            if (!--ownClassNameCount) {
-                needsNthChild = true;
-                break;
-            }
-        }
-    }
-
-    var result = nodeName.toLowerCase();
-    if (isTargetNode && nodeName.toLowerCase() === "input" && node.getAttribute("type") && !node.getAttribute("id") && !node.getAttribute("class"))
-        result += "[type=\"" + node.getAttribute("type") + "\"]";
-    if (needsNthChild) {
-        result += ":nth-child(" + (ownIndex + 1) + ")";
-    } else if (needsClassNames) {
-        for (var prefixedName in prefixedOwnClassNamesArray)
-            // for (var prefixedName in prefixedOwnClassNamesArray.keySet())
-            result += "." + escapeIdentifierIfNeeded(prefixedOwnClassNamesArray[prefixedName].substr(1));
-    }
-
-    return new UTILS.DOMNodePathStep(result, false);
-}
-
-/**
- * @constructor
- * @param {string} value
- * @param {boolean} optimized
- */
-UTILS.DOMNodePathStep = function (value, optimized) {
-    this.value = value;
-    this.optimized = optimized || false;
-}
-
-UTILS.DOMNodePathStep.prototype = {
-    /**
-     * @return {string}
-     */
-    toString: function () {
-        return this.value;
-    }
-}
-
 /* Recorder class */
 class Recorder {
     constructor() {
@@ -226,7 +7,7 @@ class Recorder {
         window.addEventListener('click', this.doClick.bind(this));
         window.addEventListener('input', this.doInput.bind(this));
 
-        /* Keep track of which element the contextmenu refers to*/
+        /* Keep track of which element the contextmenu refers to */
         this.contextMenuElement = document.body;
         window.addEventListener("contextmenu", (e) => {
             this.contextMenuElement = e.srcElement;
@@ -240,15 +21,19 @@ class Recorder {
             this[request.action](request);
         });
 
-        /* Get items from already started pages */
-        chrome.runtime.sendMessage({ action: "get_events" }, (response) => {
+        /* Get events from already started sessions */
+        chrome.runtime.sendMessage({
+            action: "get_events"
+        }, (response) => {
             this.events = response.events;
         });
     }
 
     addEvent(e) {
         this.events.push(e);
-        chrome.runtime.sendMessage({ events: this.events });
+        chrome.runtime.sendMessage({
+            events: this.events
+        });
     }
 
     doClick(e) {
@@ -326,6 +111,7 @@ class Recorder {
     }
 
     start(request) {
+        /* Clear events */
         this.events = [];
         this.started = true;
         this.addEvent({
@@ -335,7 +121,7 @@ class Recorder {
     }
 
     stop(request) {
-        var rendered = this.render();
+        const rendered = this.render();
         /* Copy result to clipboard */
         document.oncopy = (event) => {
             event.clipboardData.setData("Text", rendered);
@@ -344,41 +130,41 @@ class Recorder {
         document.execCommand("Copy");
         document.oncopy = undefined;
 
-        /* clear events */
+        /* Clear events */
         this.started = false;
         this.events = [];
     }
 
     render() {
-        var rendered = [];
+        const rendered = [];
         this.events.forEach((event, i) => {
             switch (event.type) {
                 case 'start':
-                    rendered.push(`  casper.start('${event.value}');`);
+                    rendered.push(`casper.start('${event.value}');`);
                     break;
                 case 'doClick':
-                    rendered.push(`  casper.then(function () {casper.click('${event.target}');});`);
+                    rendered.push(`casper.then(function () {casper.click('${event.target}');});`);
                     break;
                 case 'doInput':
-                    rendered.push(`  casper.then(function () {casper.sendKeys('${event.target}', '${event.value}');});`);
+                    rendered.push(`casper.then(function () {casper.sendKeys('${event.target}', '${event.value}');});`);
                     break;
                 case 'checkText':
-                    rendered.push(`  casper.then(function () {test.assertSelectorHasText('${event.target}', '${event.value}');});`);
+                    rendered.push(`casper.then(function () {test.assertSelectorHasText('${event.target}', '${event.value}');});`);
                     break;
                 case 'checkElement':
-                    rendered.push(`  casper.then(function () {test.assertExists('${event.target}');});`);
+                    rendered.push(`casper.then(function () {test.assertExists('${event.target}');});`);
                     break;
                 case 'checkImage':
-                    rendered.push(`  casper.then(function () {test.assertExists('${event.target}[src="${event.value}"]');});`);
+                    rendered.push(`casper.then(function () {test.assertExists('${event.target}[src="${event.value}"]');});`);
                     break;
                 case 'checkLink':
-                    rendered.push(`  casper.then(function () {test.assertExists('${event.target}[href="${event.value}"]');});`);
+                    rendered.push(`casper.then(function () {test.assertExists('${event.target}[href="${event.value}"]');});`);
                     break;
                 case 'takeScreenshot':
-                    rendered.push(`  casper.then(function () {casper.captureSelector("screenshot${i}.png", "html");});`);
+                    rendered.push(`casper.then(function () {casper.captureSelector("screenshot${i}.png", "html");});`);
                     break;
                 case 'addComment':
-                    rendered.push(`  casper.then(function () {test.comment('${event.value}');});`);
+                    rendered.push(`casper.then(function () {test.comment('${event.value}');});`);
                     break;
             }
         });
@@ -386,10 +172,10 @@ class Recorder {
 casper.options.viewportSize = {width: 1280, height: 720};
 casper.test.begin('Placeholder', function(test) {
 ${rendered.join('\n')}
-  casper.run(function () {test.done();});
+casper.run(function () {test.done();});
 });
-`
+`;
     }
 }
 
-var recorderInstance = new Recorder();
+const recorderInstance = new Recorder();
